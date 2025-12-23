61520fca4d4d1877f882ecec9b1fa21c40dd2cce
fre3d0m1 <pupperfishh@proton.me>
2025-12-23 15:16:27 -0500

    Finish adding functional lsp and change back to tokyo-night theme

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,20 +1,31 @@
 return {
-    -- The core Mason plugin
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
     -- Bridges Mason with the built-in LSP
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "rust_analyzer" },
+                ensure_installed = { 
+			"lua_ls", 
+			"rust_analyzer",
+			"ts_ls",
+			"html",
+			"cssls",
+			"tailwindcss",
+			"svelte",
+			"graphql",
+			"emmet_ls",
+			"prismals",
+			"pyright",
+			"eslint",
+		},
             })
         end,
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			opts = {}
+		},
+    		"neovim/nvim-lspconfig",
+	}
     },
-    -- The actual LSP configurations
-    { "neovim/nvim-lspconfig" },
 }
